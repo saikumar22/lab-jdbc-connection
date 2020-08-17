c554ae9bcf592d317f61b0d6df21be729d17ced7
saikumar22 <saigoud2212@gmail.com>
2020-08-17 19:08:43 +0530

    FSJ0720A116

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,13 @@
 package controller;
 
-import java.sql.Connection;
-
+import java.io.IOException;
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
-		
+	@SuppressWarnings("static-access")
+	public static void main(String args[]) throws IOException {
 	// Fill your code
-		
+		ConnectionManager cm = new ConnectionManager();
+		cm.getConnection();			
 	}
 }
